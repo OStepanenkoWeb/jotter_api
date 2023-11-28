4580d9033a37c857bbb125673592ba00c92cf17b
Stepanenko Oleg <o.stepanenko@id-mt.ru>
2023-11-28 16:28:07 +0300

    [NO_TASK]: Поправил конфигурацию CORS

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -2,10 +2,9 @@ import { Express } from 'express';
 import { bodyParser } from '@main/middlewares/body-parser';
 import { cors } from '@main/middlewares/cors';
 import { contentType } from '@main/middlewares/content-type';
-import env from '@main/config/env';
 
 export default (app: Express): void => {
   app.use(bodyParser);
-  app.use(cors({ origin: env.uiHost, credentials: true }));
+  app.use(cors);
   app.use(contentType);
 };
